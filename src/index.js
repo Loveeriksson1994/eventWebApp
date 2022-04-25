c0fd14ef5b3d3e39f9292f83d99529041051e74d
Love Eriksson <loveeriksson1994@gmail.com>
2022-04-25 15:05:50 +0200

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import appData from '@sitevision/api/server/appData';
 import App from './components/App';
 
 router.get('/', (req, res) => {
-  const message = 'Hello, world!';
+  const message = 'Hello, fis!';
   const name = appData.get('name');
 
   res.agnosticRender(renderToString(<App message={message} name={name} />), {
