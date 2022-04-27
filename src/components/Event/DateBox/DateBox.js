8580b43cf597a0ef220475607c10406fd27d7495
Alexander <allekolsva@gmail.com>
2022-04-27 13:57:07 +0200

    Datum-funktionen
    Fungerande datum-funkion

--- src/components/Event/DateBox/DateBox.js
+++ src/components/Event/DateBox/DateBox.js
@@ -4,12 +4,18 @@ import sorttedDate from '../../App/Logik/Logik.js';
 const list = sorttedDate;
 
 const DateBox = (props) => {
+    if(props.start == props.end){
     return(
 <div className="sup-eventPuff__dateBox">
-    
-   <time> {props.start} - {props.end} </time> 
+   <time> {props.start} </time> 
 </div>
-    );
+    ); }
+    else 
+    return(
+        <div className="sup-eventPuff__dateBox">
+        <time> {props.start} - {props.end} </time> 
+     </div> 
+    )
 };
 
 export default DateBox;
